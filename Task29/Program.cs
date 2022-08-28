78e6097fc601dfb8a56b74bd6f1a6b5055b0223e
Anna <troshkova.anna@mail.ru>
2022-08-29 00:31:40 +0400

    29

---
+++ Task29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+Console.WriteLine("Задайте размер массива числом");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[N];
+for (int i = 0; i < N; i++)
+{
+    numbers[i]=Random.Shared.Next();
+Console.Write(numbers[i] + " ");
+}
+
+
